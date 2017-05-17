b9fa843c89d1ed6998fc4eb7c8d950741c2d2579
GeorgiosP <g.psarakis3800@gmail.com>
2017-05-17 13:56:20 -0400

    set up basic menu prompt for assignment 1

---
+++ src/Assn1-boiling-GeorgePsarakis.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+#include <string.h>
+#include <stdlib.h>
+
+void _selectionMenu(); 
+
+
+int main()
+{
+    _selectionMenu();
+    return 0; 
+}
+
+void _selectionMenu(){
+    printf("******************************************************\n"); 
+    printf("**            Assignment 1 Temp/Boiling             **\n"); 
+    printf("**  Please enter the degree type you want to use:   **\n"); 
+    printf("******************************************************\n"); 
+    
+}
