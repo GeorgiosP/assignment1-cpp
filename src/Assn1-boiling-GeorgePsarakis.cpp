1f9279eaee62fa643f315d462fa02b6c3a79db1a
GeorgiosP <g.psarakis3800@gmail.com>
2017-05-25 14:55:53 -0400

    added a basic menu driven interface

--- src/Assn1-boiling-GeorgePsarakis.cpp
+++ src/Assn1-boiling-GeorgePsarakis.cpp
@@ -2,19 +2,88 @@
 #include <string.h>
 #include <stdlib.h>
 
-void _selectionMenu(); 
+
+void _welcomeMenu();
+void _promptMenu(); 
+bool _checkMenuInputLength(char *input); 
+void _notValid();  
 
 
 int main()
 {
-    _selectionMenu();
+
+    char buffer[256]; 
+    bool keepGoing = true; 
+
+     _welcomeMenu();
+    do{
+        _promptMenu();
+
+        printf("\n>> ");
+        fgets(buffer, sizeof(buffer), stdin);  
+
+        buffer[strcspn(buffer, "\n")] = 0;
+        if(_checkMenuInputLength(buffer)){
+            int menuSelection = atoi(buffer); 
+            switch(menuSelection){
+                case 1: 
+                    printf("celcius"); 
+                    break; 
+                case 2: 
+                    printf("Fahrenheit"); 
+                    break; 
+                case 3: 
+                    printf("Kelvin"); 
+                    break; 
+                case 4: 
+                    printf("Exiting program..."); 
+                    keepGoing = false; 
+                    break; 
+                default: 
+                    _notValid(); 
+                
+            }
+        }
+        else{
+            _notValid(); 
+        }
+        
+
+
+
+    }while(keepGoing == true);
+
+    
     return 0; 
 }
 
-void _selectionMenu(){
+void _welcomeMenu(){
     printf("******************************************************\n"); 
     printf("**            Assignment 1 Temp/Boiling             **\n"); 
     printf("**  Please enter the degree type you want to use:   **\n"); 
     printf("******************************************************\n"); 
     
+    
+}
+
+void _notValid(){
+    printf("******************************************************\n"); 
+    printf("**             That's Not A Valid Input             **\n"); 
+    printf("******************************************************\n"); 
+} 
+
+void _promptMenu(){
+    printf("\n1. Celcius\n"); 
+    printf("2. Fahrenheit\n");
+    printf("3. Kelvin\n"); 
+    printf("4. Exit program\n");
+
+}
+
+bool _checkMenuInputLength(char *input){
+    int length = strlen(input);
+    if(length == 1)
+        return true; 
+    else
+        return false; 
 }
